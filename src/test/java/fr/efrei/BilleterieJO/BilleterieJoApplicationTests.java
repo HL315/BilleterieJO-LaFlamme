8606b599380c4425da06794328e732b4489556bb
HL315 <hectorlevesques@gmail.com>
2024-05-16 11:27:59 +0200

    Initialisation du projet

---
+++ src/test/java/fr/efrei/BilleterieJO/BilleterieJoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.efrei.BilleterieJO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilleterieJoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
